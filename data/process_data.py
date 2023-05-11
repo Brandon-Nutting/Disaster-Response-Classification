7a5e09eae1868f014af6dd7e85e6be862b9f1574
Brandons1998 <bran.swett@gmail.com>
2023-05-11 19:00:16 -0400

    Inital commit of ETL pipeline portion

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,71 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+        Handles the loading of all data.
+        
+        Parameters
+        -----------
+        messages_filepath: File path for the messages
+        
+        categories_filepath: File path for the categories
+        Returns
+        ------------
+        df : loaded dataset.    
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = messages.merge(categories, how = 'inner', on = 'id')
+    return df
+
 
 
 def clean_data(df):
-    pass
+    '''
+        Handles the cleaning of the data.
+        
+        Parameters
+        -------------
+        df : dataframe that the cleaning steps are to be performed on.     
+        Returns
+        --------------
+        df : dataframe after cleaning steps have been performed on it.
+    '''
+    categories = df['categories'].str.split(";",expand = True)
+
+    row = categories.iloc[0]
+    category_colnames = row.tolist()
+    categories.columns = category_colnames
+
+    column_map = {col : col[-1] for col in categories.columns}
+    categories = categories.rename(columns = column_map)
+
+    df.drop(columns = ['categories'], inplace = True)
+    df = pd.concat([df,categories], axis = 1)
+    
+    df.drop_duplicates(inplace = True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Saves the df parameter in the sql database under the database_filename
+    
+    Parameters
+    -------------
+        df: Dataframe to be saved.
+        
+        database_filename: Filename of the sql database that the user wants to store the df in.
+    Returns
+    --------------
+    '''  
+    engine = create_engine(database_filename)
+    df.to_sql('myTable', engine, index=False)
 
 
 def main():
